cefc0f0ad54f6e2ef37e4f9aae869dcd620de657
lmachens <leon@machens.koeln>
2021-10-04 16:55:45 +0200

    Add favorite-card story

---
+++ src/components/favorite-card/favorite-card.stories.js
@@ -0,0 +1,9 @@
+import "./favorite-card.css";
+import favoriteCard from "./favorite-card.html";
+
+export default {
+  title: "Components/Favorite Cards",
+  parameters: { layout: "centered" },
+};
+
+export const fruits = () => favoriteCard;
