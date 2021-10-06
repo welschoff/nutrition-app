07c4c376c8f550cac7ea655d4dd88b55a2193fbb
lmachens <leon@machens.koeln>
2021-10-06 14:36:06 +0200

    Add tabs files

---
+++ src/components/tabs/tabs.stories.js
@@ -0,0 +1,9 @@
+import "./tabs.css";
+import tabs from "./tabs.html";
+
+export default {
+  title: "Components/Tabs",
+  parameters: { layout: "centered" },
+};
+
+export const favorites = () => tabs;
