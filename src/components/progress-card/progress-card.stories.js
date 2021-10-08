77316010c0b139cd3807bd86d455390a1f0cdbd4
welschoff <h.welschoff@gmx.de>
2021-10-08 16:26:11 +0200

    Added progress-card to Storybook

---
+++ src/components/progress-card/progress-card.stories.js
@@ -0,0 +1,9 @@
+import "./progress-card.css";
+import progressCard from "./progress-card.html";
+
+export default {
+  title: "Components/Progress Card",
+  parameters: { layout: "centered" },
+};
+
+export const progress = () => progressCard;
